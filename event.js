2d07fbe54e366a1cdaea1f2d2eb7380e0c1e7158
hgt803 <359865238@qq.com>
2018-04-05 18:53:06 +0800

    发射事件后，才开始监听的事件无法调用

--- event.js
+++ event.js
@@ -1,13 +1,23 @@
+// 先监听，后发射事件储存对象
 var events = {};
+// 先反射，后监听事件储存对象
+var storageEvent = {};
 
 function on(name, self, callback) {
     var tuple = [self, callback];
     var callbacks = events[name];
     if (Array.isArray(callbacks)) {
         callbacks.push(tuple);
-    }
-    else {
-        events[name] = [tuple];
+    } else {
+        callbacks = storageEvent[name];
+        if (Array.isArray(callbacks)) {
+            callbacks.map((tuple) => {
+                var data = tuple[0];
+                callback.call(self, data);
+            })
+        } else {
+            events[name] = [tuple];
+        }
     }
 }
 
@@ -17,6 +27,11 @@ function remove(name, self) {
         events[name] = callbacks.filter((tuple) => {
             return tuple[0] != self;
         })
+    } else {
+        callbacks = storageEvent[name];
+        if (Array.isArray(callbacks)) {
+            delete storageEvent[name];
+        }
     }
 }
 
@@ -28,6 +43,14 @@ function emit(name, data) {
             var callback = tuple[1];
             callback.call(self, data);
         })
+    } else {
+        callbacks = storageEvent[name];
+        var tuple = [data || 'data'];
+        if (Array.isArray(callbacks)) {
+            callbacks.push(tuple);
+        } else {
+            storageEvent[name] = tuple
+        }
     }
 }
 
